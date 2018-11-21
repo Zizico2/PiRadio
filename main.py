815eb4b02a60a932f2d68d4cf48f493b88dfaab5
Bernardo António Borda d'Água <berna.agua@gmail.com>
2018-11-21 19:38:20 +0000

    mock pins

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+from gpiozero.pins.mock import MockFactory	
 import vlc
 from radioplayer import RadioPlayer
 from gpiozero import Button
 from knob import Knob
 
+
+Device.pin_factory = MockFactory()
+
 player = RadioPlayer()
-RADIOCOMERCIALButton = Button() #insert pin
-RFMButton = Button() #insert pin
-volumeKnob = Knob() # insert pins
+RADIOCOMERCIALButton = Button(22) #insert pin
+RFMButton = Button(21) #insert pin
+volumeKnob = Knob(16,17) # insert pins
 
 RFMButton.when_pressed = player.changeToRFM
 RADIOCOMERCIALButton.when_pressed = player.changeToRADIOCOMERCIAL
 volumeKnob.on_rotate_left = player.volumeDown
 volumeKnob.on_rotate_right = player.volumeUp
 #volumeKnob.on_rotate_press player.toggleMute
+
+Device.pin_factory.pin(22).drive_low()
+
 
 
 #while True:
